0fb52a036a517191e73600165a181aba515994b9
AlejoGomezDev <alejongomez@gmail.com>
2025-03-27 23:18:22 -0300

    segundo aporte

--- src/ejercicio1/Empleado.java
+++ src/ejercicio1/Empleado.java
@@ -1,9 +1,31 @@
 package ejercicio1;
 
 public class Empleado {
- private int id;
+	
+ private final int id;
  private String nombre;
  private int edad;
- private static int contadorId = 1000;
+ 
+ private static int contadorId = 999;
+ 
+ 	//GETTERS & SETTERS
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
 	
 }
