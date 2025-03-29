c94194245d207bace38b2eb750866e721d32d1bf
liocoma <lionelcorradini@gmail.com>
2025-03-28 22:15:21 -0300

    Principal

---
+++ src/ejercicio1/Principal.java
@@ -0,0 +1,15 @@
+package ejercicio1;
+
+public class Principal {
+	
+	public static void main(String[] args) {
+		
+		Empleado[] vEmp = new Empleado [5];
+
+		//Asignacion de Empleados
+		vEmp[0] = new Empleado("Jorge", 30);
+		vEmp[1] = new Empleado();
+
+	}
+
+}
